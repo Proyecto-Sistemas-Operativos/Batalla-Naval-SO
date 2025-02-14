2db6b1c7c315738362d6443322de1ded5259fd66
Sans <167203085+Sansalmao@users.noreply.github.com>
2025-02-14 14:55:36 -0400

    Update funcional.py

--- funcional.py
+++ funcional.py
@@ -2,15 +2,18 @@ import random
 
 print("\nBATALLA NAVAL\n")
 
+# Crear el tablero de juego
 def crear_tablero(tamaño):
     return [['~'] * tamaño for _ in range(tamaño)]
 
+# Imprimir los tableros de juego en paralelo
 def imprimir_tableros(tablero1, tablero2, tipo1, tipo2):
     print(f"\n    {tipo1}          {tipo2}\n")
     print("   A B C D E F G H I J        A B C D E F G H I J")
     for idx, (fila1, fila2) in enumerate(zip(tablero1, tablero2)):
         print(f"{idx+1:<2} " + " ".join(fila1) + "     " + f"{idx+1:<2} " + " ".join(fila2))
 
+# Colocar los barcos en el tablero
 def colocar_barcos_manual(tablero, tamaño, nombre_barco):
     while True:
         orientacion = input(f"\nIntroduce la orientación del {nombre_barco} (H para horizontal, V para vertical): ").upper()
@@ -34,6 +37,7 @@ def colocar_barcos_manual(tablero, tamaño, nombre_barco):
                 break
         print("Posición inválida o el espacio ya está ocupado. Inténtalo de nuevo.")
 
+# Realizar un disparo
 def disparar(tablero_jugador, tablero_disparos, fila, columna):
     if tablero_jugador[fila][columna] == 'B':
         tablero_jugador[fila][columna] = 'X'
@@ -45,6 +49,7 @@ def disparar(tablero_jugador, tablero_disparos, fila, columna):
         return False
     return None
 
+# Verificar si un barco está hundido
 def verificar_hundimiento(tablero, tamaños_barcos):
     for tamaño in tamaños_barcos:
         for fila in tablero:
@@ -53,62 +58,77 @@ def verificar_hundimiento(tablero, tamaños_barcos):
                     return True, tamaño
     return False, 0
 
+# Juego principal
 def juego_batalla_naval():
     tamaño = 10
-    barcos = [("Portaviones", 4), ("Acorazado", 3), ("Crucero", 2), ("Submarino", 1)]  # Nombre y tamaño de los barcos
+    barcos = [("Portaviones", 4), ("Acorazado", 3), ("Crucero", 2), ("Destructor", 1)]  # Nombre y tamaño de los barcos
     tamaños_barcos = [4, 3, 2, 1]  # Solo tamaños de los barcos para verificación
 
-    tablero_jugador1_barcos = crear_tablero(tamaño)
-    tablero_jugador1_disparos = crear_tablero(tamaño)
-    tablero_jugador2_barcos = crear_tablero(tamaño)
-    tablero_jugador2_disparos = crear_tablero(tamaño)
+    victorias_jugador1 = 0
+    victorias_jugador2 = 0
 
-    print("Jugador 1, coloca tus barcos.")
-    for nombre_barco, tamaño in barcos:
-        imprimir_tableros(tablero_jugador1_barcos, tablero_jugador1_disparos, "Barcos Jugador 1", "Disparos Jugador 1")
-        colocar_barcos_manual(tablero_jugador1_barcos, tamaño, nombre_barco)
-
-    print("Jugador 2, coloca tus barcos.")
-    for nombre_barco, tamaño in barcos:
-        imprimir_tableros(tablero_jugador2_barcos, tablero_jugador2_disparos, "Barcos Jugador 2", "Disparos Jugador 2")
-        colocar_barcos_manual(tablero_jugador2_barcos, tamaño, nombre_barco)
-
-    turno = 1
     while True:
-        print(f"\nTurno del Jugador {turno}")
-        if turno == 1:
+        tablero_jugador1_barcos = crear_tablero(tamaño)
+        tablero_jugador1_disparos = crear_tablero(tamaño)
+        tablero_jugador2_barcos = crear_tablero(tamaño)
+        tablero_jugador2_disparos = crear_tablero(tamaño)
+
+        print("Jugador 1, coloca tus barcos.")
+        for nombre_barco, tamaño in barcos:
             imprimir_tableros(tablero_jugador1_barcos, tablero_jugador1_disparos, "Barcos Jugador 1", "Disparos Jugador 1")
-        else:
+            colocar_barcos_manual(tablero_jugador1_barcos, tamaño, nombre_barco)
+
+        print("\nJugador 2, coloca tus barcos.")
+        for nombre_barco, tamaño in barcos:
             imprimir_tableros(tablero_jugador2_barcos, tablero_jugador2_disparos, "Barcos Jugador 2", "Disparos Jugador 2")
+            colocar_barcos_manual(tablero_jugador2_barcos, tamaño, nombre_barco)
 
-        posicion = input("Introduce la posición para disparar (ej. B3): ")
-        columna = ord(posicion[0].upper()) - ord('A')
-        fila = int(posicion[1:]) - 1
+        turno = 1
+        while True:
+            print(f"\nTurno del Jugador {turno}")
+            if turno == 1:
+                imprimir_tableros(tablero_jugador1_barcos, tablero_jugador1_disparos, "Barcos Jugador 1", "Disparos Jugador 1")
+            else:
+                imprimir_tableros(tablero_jugador2_barcos, tablero_jugador2_disparos, "Barcos Jugador 2", "Disparos Jugador 2")
 
-        if turno == 1:
-            resultado = disparar(tablero_jugador2_barcos, tablero_jugador1_disparos, fila, columna)
-            hundido, tamaño = verificar_hundimiento(tablero_jugador2_barcos, tamaños_barcos)
-        else:
-            resultado = disparar(tablero_jugador1_barcos, tablero_jugador2_disparos, fila, columna)
-            hundido, tamaño = verificar_hundimiento(tablero_jugador1_barcos, tamaños_barcos)
+            posicion = input("Introduce la posición para disparar (ej. B3): ")
+            columna = ord(posicion[0].upper()) - ord('A')
+            fila = int(posicion[1:]) - 1
 
-        if resultado is None:
-            print("Ya disparaste en esa posición.")
-        elif resultado:
-            print("¡Tocado!")
-            if hundido:
-                print(f"¡Has hundido un barco de tamaño {tamaño}!")
-        else:
-            print("¡Agua!")
+            if turno == 1:
+                resultado = disparar(tablero_jugador2_barcos, tablero_jugador1_disparos, fila, columna)
+                hundido, tamaño = verificar_hundimiento(tablero_jugador2_barcos, tamaños_barcos)
+            else:
+                resultado = disparar(tablero_jugador1_barcos, tablero_jugador2_disparos, fila, columna)
+                hundido, tamaño = verificar_hundimiento(tablero_jugador1_barcos, tamaños_barcos)
 
-        if all(celda != 'B' for fila in tablero_jugador2_barcos for celda in fila):
-            print("¡Felicidades, hundiste todos los barcos!")
-            print("¡Jugador 1 gana!")
+            if resultado is None:
+                print("Ya disparaste en esa posición.")
+            elif resultado:
+                print("¡Le diste!")
+                if hundido:
+                    print(f"¡Has hundido un barco de tamaño {tamaño}!")
+            else:
+                print("¡Agua!")
+
+            if all(celda != 'B' for fila in tablero_jugador2_barcos for celda in fila):
+                print("¡Felicidades, hundiste todos los barcos!")
+                print("¡Jugador 1 gana!")
+                victorias_jugador1 += 1
+                break
+            elif all(celda != 'B' for fila in tablero_jugador1_barcos for celda in fila):
+                print("¡Felicidades, hundiste todos los barcos!")
+                print("¡Jugador 2 gana!")
+                victorias_jugador2 += 1
+                break
+
+            turno = 2 if turno == 1 else 1
+
+        print(f"\nPuntuación actual:\nJugador 1: {victorias_jugador1} victorias\nJugador 2: {victorias_jugador2} victorias")
+
+        continuar = input("\n¿Quieres jugar otra partida? (s/n): ").lower()
+        if continuar != 's':
             break
-        elif all(celda != 'B' for fila in tablero_jugador1_barcos for celda in fila):
-            print("¡Felicidades, hundiste todos los barcos!")
-            print("¡Jugador 2 gana!")
 
-        turno = 2 if turno == 1 else 1
-
+# Ejecutar el juego
 juego_batalla_naval()
